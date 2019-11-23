a6b8646e4f3b6453789e5d4ebd01aa402bc739df
michikogo <37466954+michikogo@users.noreply.github.com>
2019-11-23 17:37:26 +0800

    left brace error checking

--- CMPILERParser/src/main.java
+++ CMPILERParser/src/main.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 public class main {
     public static void main(String[] args) throws IOException {
         // GUI
-        mainInterface frame = new mainInterface();
-        frame.setVisible(true);
+//        mainInterface frame = new mainInterface();
+//        frame.setVisible(true);
 
         //TODO: Contents of runBtn will be transferred to mainInterface Class
         gLexer lexer = new gLexer(new ANTLRFileStream("D:\\GitHub\\CompilerProject\\CMPILERParser\\testcases\\interpreter_test.txt"));
+        lexer.removeErrorListeners();
         gParser parser = new gParser(new CommonTokenStream(lexer));
+        parser.removeErrorListeners();
+
         ParseTree tree = parser.program();
 
         ParseTreeWalker walker = new ParseTreeWalker();
